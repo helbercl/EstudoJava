625e4f35abb2de63d074ee966bdb8c3e40aa4ee4
Helber Cardoso <info.helber@gmail.com>
2018-11-19 01:00:51 -0200

    refs 191120180000 complementando commit anterior

---
+++ src/br/com/algaworks/javabasico/OperadoresAritmeticos.java
@@ -0,0 +1,20 @@
+package br.com.algaworks.javabasico;
+
+
+public class OperadoresAritmeticos {
+
+	public static void main(String[] args) {
+		//preço dos 3 ultimos produtos vendidos
+	  int produto1 =5;
+	  int produto2 = 8;
+	  int produto3 =12;
+	  
+	  int media = (produto1+produto2+produto3)/3;
+	  System.out.println("\" Sistema de Calculo de Média\"");
+	  System.out.println("Preco 1 "+produto1);
+	  System.out.println("Preco 2 "+produto2);
+	  System.out.println("Preco 3 "+produto3);
+	  System.out.println(" Média de Preços: "+media);
+	}
+
+}
