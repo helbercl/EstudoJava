8042f110a12eb51b25fe7b00bcd005c12b1a0b09
Helber Cardoso <info.helber@gmail.com>
2018-11-19 00:45:06 -0200

    refs 191120181144 estudei novamente conceitos basicos ate operadores logicos

--- src/Escape.java
+++ src/Escape.java
@@ -1,17 +1,29 @@
 
 public class Escape {
-	
-	public static void main(String[] args){
-		
+
+	public static void main(String[] args) {
+
 		// contrabarra em frente a aspas duplas faz com que ignore/escape o caracter
 		System.out.println("Como fazer \"escape\" com aspas duplas!");
-		// por duas contrabarras em frente ignore/escape o caracter imprimindo uma contrabarra
+		// por duas contrabarras em frente ignore/escape o caracter imprimindo uma
+		// contrabarra
 		System.out.println("Imprimir \\barra \\invertida\\!");
 		// \n pula uma linha
 		System.out.println("Contrabarra \n N");
-		
-		
-		
+		// contrabarra igonora a aspas duplas como inicio de nova String
+		System.out.println("Oi \"Mundo\"");
+		// duas contras barras faz com que seja impresso uma contra barra
+		System.out.println("\\ contrabarra invertida\\");
+		// \n pula uma linha
+
+		System.out.println("Oi \n mundo");
+
+		// Escapes conhecidos no java
+		System.out.println("backspace \b backspace");
+		System.out.println("Tabulação \t Horizontal");
+		System.out.println("Form \f Feed");
+
+
 	}
 
 }
