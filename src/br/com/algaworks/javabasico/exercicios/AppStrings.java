1009ee2f622e996edfad648132a8bc1dd2d76ff4
Helber Cardoso <info.helber@gmail.com>
2018-11-24 11:03:29 -0200

    refs 241120181103 atualização geral

--- src/br/com/algaworks/javabasico/exercicios/AppStrings.java
+++ src/br/com/algaworks/javabasico/exercicios/AppStrings.java
@@ -9,9 +9,15 @@ public class AppStrings {
    System.out.println("a) Numero Caracteres :"+ digitada.length());
    System.out.println("b) Todas Letras Maiusculas :" + digitada.toUpperCase());
    System.out.println("c) Numero de Vogais da String : ");
-  // System.out.println("d) String começa com UNI (ignorando maiúsculas/minúsculas) ? " + digitada.equalsIgnoreCase(digitada.startsWith("UNI"));
+   System.out.println("d) Se a String começa com UNI (ignorando maiúsculas/minúsculas) ? " );
+   //return boolean startWith com parametro String
+   //return boolean equalsIgnoreCase  com parametro String
+   digitada.equalsIgnoreCase(digitada);
+   
+	//int result= condicao?value1:value2;
+
    System.out.println("e) String digitada termina com “RIO” (ignorando maiúsculas/minúsculas)");
- //  System.out.println("f) O número de dígitos (0 a 9) da string " + digitada.  );
+   System.out.println("f) O número de dígitos (0 a 9) da string "   );
    System.out.println("g) String é um palíndromo ou não");
 
 	}
