c53f7e09c0e4dbecaf87943964fafc04335db65c
Helber Cardoso Lemos <info.helber@gmail.com>
2019-03-27 18:30:34 -0300

    refs 270320191830 estudo wrappers e boxing - exercicio

--- src/br/com/algaworks/javabasico/exercicios/Televisor.java
+++ src/br/com/algaworks/javabasico/exercicios/Televisor.java
@@ -1,25 +1,32 @@
 package br.com.algaworks.javabasico.exercicios;
 
+import java.util.Scanner;
+
 public class Televisor {
 
 	public static void main(String[] args) {
-		
-		Integer canal=130;
-		Integer volume=20;
-		
-		
-	  public void mudarCanal(Integer novoCanal) {
+		Integer canal=0;Integer volume=0;
+		while (canal >= 0) {
 			
-			if (canal==novoCanal) {
-				System.out.println("Novo canal é também o canal atual.");
-			} else {
-				canal=novoCanal;
-			    System.out.println("Canal Alterado para " + canal);
-			}
+			Scanner entrada = new Scanner(System.in);
+			System.out.println("Informe o novo canal:");
+			canal = entrada.nextInt();
+			System.out.println("Informe o volume:");
+			volume = entrada.nextInt();
+			mudarCanal(canal);
 		}
-	
-		
-	
+
+	}
+
+	static void mudarCanal(Integer canal) {
+		Integer novoCanal = null;		
+		if (novoCanal == canal) {
+			System.out.println("Novo canal é também o canal atual.");
+			
+		} else {
+			novoCanal = canal;
+			System.out.println("Canal Alterado para " + canal);
+		}
 	}
 
 }
